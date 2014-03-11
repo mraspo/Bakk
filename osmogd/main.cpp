c8a25509bfe6c46da5f2b5b054e437a0ef3cce67
osmogdsync <michael.raspotnig@gmx.at>
2014-03-11 19:49:21 +0100

    write shapefile

--- osmogd/main.cpp
+++ osmogd/main.cpp
@@ -9,6 +9,7 @@
 
 #include <iostream>
 #include "ogr_api.h"
+#include "ogr_spatialref.h"
 
 
 using namespace std;
@@ -16,6 +17,9 @@ using namespace std;
 
 int main()
 {
+    /*
+    //reading from shapefile
+
     //register all format drivers built into GDAL/OGR -logrsf_frmts, -fexceptions -logrsf_frmts
     OGRRegisterAll();
 
@@ -81,7 +85,7 @@ int main()
         //if its a point print it
         if( hGeometry != NULL && wkbFlatten(OGR_G_GetGeometryType(hGeometry)) == wkbPoint )
         {
-            printf( "%.3f,%3.f\n", OGR_G_GetX(hGeometry, 0), OGR_G_GetY(hGeometry, 0) );
+            printf( "%.7f,%.7f\n", OGR_G_GetX(hGeometry, 0), OGR_G_GetY(hGeometry, 0) );
         }
         else
         {
@@ -93,6 +97,99 @@ int main()
 
     }
     OGR_DS_Destroy( hDS );
+    */
+
+
+
+    //reading from and writing to shapefile
+
+    double x, y;
+    char szName[33];
+
+    //shapefile driver
+    const char *pszDriverName = "ESRI Shapefile";
+    OGRSFDriverH hDriver;
+
+    OGRRegisterAll();
+
+    hDriver = OGRGetDriverByName( pszDriverName );
+    if( hDriver == NULL )
+    {
+        printf( "%s driver not available.\n", pszDriverName );
+        exit( 1 );
+    }
+
+    //create shapefile
+    OGRDataSourceH hDS;
+
+    hDS = OGR_Dr_CreateDataSource( hDriver, "point_out.shp", NULL );
+    if( hDS == NULL )
+    {
+        printf( "Creation of output file failed.\n" );
+        exit( 1 );
+    }
+
+    //create layer, point, no reference system information
+    OGRLayerH hLayer;
+
+    OGRSpatialReference oSRS;
+
+    oSRS.SetWellKnownGeogCS( "WGS84" );
+//SRS_PT_CYLINDRICAL_EQUAL_AREA
+
+//(OGRSpatialReferenceH *) oSRS SRS_DN_WGS84
+    hLayer = OGR_DS_CreateLayer( hDS, "point_out", (OGRSpatialReferenceH *) oSRS , wkbPoint, NULL );
+    if( hLayer == NULL )
+    {
+        printf( "Layer creation failed.\n" );
+        exit( 1 );
+    }
+
+    //create field
+    OGRFieldDefnH hFieldDefn;
+
+    hFieldDefn = OGR_Fld_Create( "Name", OFTString );
+
+    //field width
+    OGR_Fld_SetWidth( hFieldDefn, 254); //32
+
+    if( OGR_L_CreateField( hLayer, hFieldDefn, TRUE ) != OGRERR_NONE )
+    {
+        printf( "Creating Name field failed.\n" );
+        exit( 1 );
+    }
+
+    OGR_Fld_Destroy(hFieldDefn);
+
+    //reading lines of the form "x,y,name" from stdin, and parsing them
+    while( !feof(stdin) && fscanf( stdin, "%lf,%lf,%32s", &x, &y, szName ) == 3 )
+    {
+        //write feature to disk, set attributes and attach geometry before trying to write it to the layer
+        OGRFeatureH hFeature;
+        OGRGeometryH hPt;
+
+        hFeature = OGR_F_Create( OGR_L_GetLayerDefn( hLayer ) );
+        OGR_F_SetFieldString( hFeature, OGR_F_GetFieldIndex(hFeature, "Name"), szName );
+
+        //local geometry object, assign copy to the feature
+        hPt = OGR_G_CreateGeometry(wkbPoint);
+        OGR_G_SetPoint_2D(hPt, 0, x, y);
+
+        OGR_F_SetGeometry( hFeature, hPt );
+        OGR_G_DestroyGeometry(hPt);
+
+        //create feature in the file
+        if( OGR_L_CreateFeature( hLayer, hFeature ) != OGRERR_NONE )
+        {
+           printf( "Failed to create feature in shapefile.\n" );
+           exit( 1 );
+        }
+
+        OGR_F_Destroy( hFeature );
+   }
+
+   //close down datasource
+   OGR_DS_Destroy( hDS );
 
     return 0;
 }
